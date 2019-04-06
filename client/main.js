fc20a676e73b63812e59372e0d38d958dc99ddfe
Mike Acre <mikeacre@gmail.com>
2019-04-06 11:07:51 -0400

    init

---
+++ client/main.js
@@ -0,0 +1,12 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+
+import 'jquery';
+import 'jquery-ui'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-select'
+import 'bootstrap-select/sass/bootstrap-select.scss'
+import './main.html';
+
